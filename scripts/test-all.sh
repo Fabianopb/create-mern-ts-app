9daecabc43debb73ca75c97d2a21704e6f309892
Fabiano Brito <fabianopbrito@gmail.com>
2018-05-31 19:01:21 -0300

    Exits test when it fails so Travis also fails.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
+safeRunCommand() {
+  typeset cmnd="$*"
+  typeset ret_code
 
+  echo cmnd=$cmnd
+  eval $cmnd
+  ret_code=$?
+  if [ $ret_code != 0 ]; then
+    printf "Error : [%d] when executing command: '$cmnd'" $ret_code
+    exit $ret_code
+  fi
+}
+
+command="yarn test"
+
+# script starts here
 cd backend/
+
 echo "testing backend..."
-yarn test
+safeRunCommand "$command"
 
 cd ../frontend
+
 echo "testing frontend..."
-yarn test
+safeRunCommand "$command"

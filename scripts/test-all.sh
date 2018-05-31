ee7e3eb82dfba16321c7ea5808538e8f8265856d
Fabiano Brito <fabianopbrito@gmail.com>
2018-05-31 19:28:51 -0300

    Uses simple yarn script to test all.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
-safeRunCommand() {
-  typeset cmnd="$*"
-  typeset ret_code
-
-  echo cmnd=$cmnd
-  eval $cmnd
-  ret_code=$?
-  if [ $ret_code != 0 ]; then
-    printf "Error : [%d] when executing command: '$cmnd'" $ret_code
-    exit $ret_code
-  fi
-}
-
-command="yarn test"
 
 # script starts here
 cd backend/
 
 echo "testing backend..."
-safeRunCommand "$command"
+yarn test
 
 cd ../frontend
 
 echo "testing frontend..."
-safeRunCommand "$command"
+yarn test

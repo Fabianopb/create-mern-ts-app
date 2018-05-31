09b32120c674798fd78b3a8933bd894f5bf8c3a0
Fabiano Brito <fabianopbrito@gmail.com>
2018-05-31 18:44:20 -0300

    Fails test to check integration with Travis.

--- backend/tests/home.test.ts
+++ backend/tests/home.test.ts
@@ -6,7 +6,7 @@ import * as app from "../server/server";
 describe("GET /api", () => {
   it("should return 200 OK", () => {
     return request(app)
-      .get("/api")
+      .get("/")
       .expect(200)
       .then(res => {
         expect(res.body).have.property("message");

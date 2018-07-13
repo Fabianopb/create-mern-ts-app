9365dffffa4e864c9c4695a73fdbaac3d1545871
Fabiano Brito <fabianopbrito@gmail.com>
2018-07-13 14:01:52 +0300

    Simplifies root scripts and upgrades frontend.

--- scripts/build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd backend/
-echo "building backend..."
-yarn build
-
-cd ../frontend
-echo "building frontend..."
-yarn build
--- scripts/install-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd backend/
-echo "installing backend packages..."
-yarn install
-
-cd ../frontend
-echo "installing frontend packages..."
-yarn install

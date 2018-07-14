5dbb5b25a1ece3726976ea1296ed8cb765652616
Fabiano Brito <fabianopbrito@gmail.com>
2018-07-14 15:27:05 +0300

    Removes comments.

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -12,21 +12,18 @@ module.exports = {
   entry: './server/server.ts',
   output: {
     path: __dirname + '/dist',
-    filename: 'server.js',
+    filename: 'server.js'
   },
   resolve: {
-    // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
   module: {
     loaders: [
-      // All files with a '.ts' or '.tsx'
-      // extension will be handled by 'ts-loader'
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
-      },
-    ],
+      }
+    ]
   },
   target: 'node',
   externals: nodeModules,

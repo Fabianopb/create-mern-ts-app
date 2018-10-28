b522a078cd4509cad800c31f8dd8c811d802fff4
Fabiano Brito <fabianopbrito@gmail.com>
2018-10-28 14:34:48 +0200

    Fix lint errors in webpack config.

--- template/backend/webpack.config.js
+++ template/backend/webpack.config.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 
 const nodeModules = {};
@@ -13,17 +14,17 @@ fs.readdirSync('node_modules')
 module.exports = {
   entry: './server/server.ts',
   output: {
-    path: __dirname + '/build',
+    path: path.join(__dirname, '/build'),
     filename: 'server.js'
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
     loaders: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: 'ts-loader'
       }
     ]
   },

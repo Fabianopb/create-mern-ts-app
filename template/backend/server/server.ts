1f28724c54213163864a9d73897a71a32093a6a7
Fabiano Brito <fabianopbrito@gmail.com>
2018-10-28 15:15:21 +0200

    Use url variable to define db name.

--- template/backend/server/server.ts
+++ template/backend/server/server.ts
@@ -4,14 +4,11 @@ import app from "./app";
 import User from "./users/user.model";
 
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/test_database";
-const dbName = url.split("/")[url.split("/").length - 1];
 const port = process.env.PORT || 9000;
 
 (async () => {
   // Connect to the database
-  const client = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test_database", {
-    useNewUrlParser: true
-  });
+  const client = await mongoose.connect(url, { useNewUrlParser: true });
   // Populate database with sample data if it's empty
   await populateDatabase();
   // Start express App
